bfa3a7d06b6242808d4313a22b24c351541b74fd
Mahedul-Karim <karimrupo21@gmail.com>
2025-08-23 08:42:35 +0600

    initial project setup done

---
+++ src/components/common/loader/LinearProgress.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { createPortal } from "react-dom";
+
+const LinearProgress = () => {
+  const progress = (
+    <div className="h-1 fixed w-full top-0 left-0 z-[99999]">
+      <div className="progress-bar-long left-0 right-[100%] top-0 bottom-0 absolute bg-primary-foreground rounded-md"></div>
+      <div className="progress-bar-short left-0 right-[100%] top-0 bottom-0 absolute bg-primary-foreground rounded-md"></div>
+    </div>
+  );
+
+  return createPortal(progress, document?.body);
+};
+
+export default LinearProgress;
