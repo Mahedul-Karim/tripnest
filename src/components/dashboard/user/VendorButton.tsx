efbebe4be771de7340c93e15b90910ea3f0eac7a
Mahedul-Karim <karimrupo21@gmail.com>
2025-09-01 17:10:15 +0600

    vendor account creation method added

--- src/components/dashboard/user/VendorButton.tsx
+++ src/components/dashboard/user/VendorButton.tsx
@@ -1,21 +1,40 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useCtx } from "@/context/Context"; 
+import { useCtx } from "@/context/Context";
 import { toast } from "sonner";
-// import { requestForVendor } from "@/lib/actions/user";
 import LinearProgress from "@/components/common/loader/LinearProgress";
+import { requestForVendor } from "@/lib/actions/user";
+import { useMutation } from "@tanstack/react-query";
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 const VendorButton: React.FC<Props> = ({ className }) => {
   const router = useRouter();
 
-  const [isLoading, setIsLoading] = useState(false);
-
   const { user, setUser } = useCtx();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const data = await requestForVendor(user?.email as string);
+
+      if (!data.success) {
+        throw new Error(data.message);
+      }
+
+      return data;
+    },
+    onSuccess: (data) => {
+      //@ts-ignore
+      setUser(data.user);
+      toast.success("Vendor account created successfully!");
+    },
+    onError: (err: any) => {
+      toast.error(err.message);
+    },
+  });
 
   const handleOnClick = async () => {
     if (user?.role === "pending") {
@@ -28,33 +47,17 @@ const VendorButton: React.FC<Props> = ({ className }) => {
       return;
     }
 
-    try {
-      setIsLoading(true);
-
-    //   const data = await requestForVendor(user?.email as string);
-
-    //   if (!data.success) {
-    //     throw new Error(data.message);
-    //   }
-
-    //   setUser(data.user);
-
-      toast.success("Vendor account created successfully!");
-    } catch (err: any) {
-      toast.error(err.message);
-    } finally {
-      setIsLoading(false);
-    }
+    mutate();
   };
   return (
     <div className={`${className}`}>
-      {isLoading && <LinearProgress />}
+      {isPending && <LinearProgress />}
       <Button
         type="button"
         className={`w-full bg-white hover:bg-white border-primary text-xs sm:text-sm sm:px-4 px-2 md:py-2 py-1 text-primary hover:text-primary disabled:border-disabled disabled:text-disabled`}
         variant={"outline"}
         onClick={handleOnClick}
-        disabled={isLoading}
+        disabled={isPending}
       >
         {user?.role === "user" && "Become a vendor"}
 
